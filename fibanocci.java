c2b5b2eda729e3c3386ca545d6752cbb2d8cb8d0
Theertha <109263570+theerthaavnsh@users.noreply.github.com>
2023-11-06 09:58:43 +0530

    Update fibanocci.java

---
+++ fibanocci.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of Fibonacci series terms: ");
+        int n = scanner.nextInt();
+        scanner.close();
+
+        System.out.println("Fibonacci Series up to " + n + " terms:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
